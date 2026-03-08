23bc44ec8c0fee29ca23f5abd20009a6f61e4640
Alfie <A.Lea3263@student.leedsbeckett.ac.uk>
2026-03-08 13:50:20 +0000

    Update main entry point and initialize providers

--- App/team_app/lib/main.dart
+++ App/team_app/lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'app.dart';
 import 'services/auth_service.dart';
+import 'firebase_options.dart';
 
-void main(){
-runApp( 
-MultiProvider( 
-providers: [
-ChangeNotifierProvider(create: (_) => AuthService()),
-],
-child: const MyApp(),
-),
-);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthService()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
