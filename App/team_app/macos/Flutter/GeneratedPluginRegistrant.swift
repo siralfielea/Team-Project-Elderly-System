b5bdbc6dd4bc118cf09008bffe99a581b3c3aa1f
Alfie <A.Lea3263@student.leedsbeckett.ac.uk>
2026-03-08 13:50:40 +0000

    Update generated plugin registrants

--- App/team_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ App/team_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,28 @@
 import FlutterMacOS
 import Foundation
 
+import desktop_webview_window
+import firebase_auth
+import firebase_core
+import flutter_secure_storage_darwin
+import flutter_web_auth_2
+import geolocator_apple
+import package_info_plus
+import shared_preferences_foundation
+import sqflite_darwin
+import url_launcher_macos
+import window_to_front
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DesktopWebviewWindowPlugin.register(with: registry.registrar(forPlugin: "DesktopWebviewWindowPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowToFrontPlugin.register(with: registry.registrar(forPlugin: "WindowToFrontPlugin"))
 }
